c4579b97a27ca921a139fdbf301dd67ecd26c945
Liberathor <juancarlosmillos21@gmail.com>
2015-04-14 04:16:09 -0500

    feature: send chat messages added

---
+++ android/src/main/java/co/com/widetech/mamut/android/adapter/ChatAdapter.java
@@ -0,0 +1,69 @@
+package co.com.widetech.mamut.android.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import co.com.widetech.mamut.android.R;
+import com.co.widetech.serial_port_core.models.Chat;
+
+import java.util.Vector;
+
+public class ChatAdapter extends BaseAdapter {
+    private static LayoutInflater inflater = null;
+    private Vector<Chat> messages;
+
+    public ChatAdapter(Vector<Chat> messages, Context context) {
+        this.messages = messages;
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
+    }
+
+    public int getCount() {
+        return this.messages.size();
+    }
+
+    public Object getItem(int position) {
+        return this.messages.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Chat c = this.messages.get(position);
+        String name = c.getUser();
+        String message = c.getMessage();
+
+        int index = message.indexOf(",");
+        message = message.substring(index + 1, message.length());
+
+        String hour = c.getDate();
+
+        View vi = convertView;
+
+        if (convertView == null)
+            if (name.equalsIgnoreCase("Central"))
+                vi = inflater.inflate(R.layout.message_row_pcc, null);
+            else
+                vi = inflater.inflate(R.layout.message_row, null);
+
+        TextView textName = (TextView) vi.findViewById(R.id.NameUser);
+        TextView textMessage = (TextView) vi.findViewById(R.id.MessageUser);
+        TextView textHour = (TextView) vi.findViewById(R.id.HourMessage);
+
+        textName.setText(name + ":");
+        textMessage.setText(message);
+        textHour.setText(hour);
+
+        return vi;
+    }
+
+}
--- android/src/main/java/co/com/widetech/mamut/android/view/ChatActivity.java
+++ android/src/main/java/co/com/widetech/mamut/android/view/ChatActivity.java
@@ -1,25 +1,64 @@
 package co.com.widetech.mamut.android.view;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import co.com.widetech.mamut.android.R;
-import utils.Config;
-import utils.MessageBuilder;
+import co.com.widetech.mamut.android.adapter.ChatAdapter;
+import co.com.widetech.mamut.android.utils.Config;
+import co.com.widetech.mamut.android.utils.MessageBuilder;
+import com.co.widetech.serial_port_core.models.Chat;
+import com.co.widetech.serial_port_core.service.TransportDataService;
+import com.co.widetech.serial_port_core.tools.WideTechTools;
+
+import java.util.Vector;
 
 public class ChatActivity extends BinderServiceActivity {
+    private Vector<Chat> messages;
+    private int sizechat;
+    private StatusChatActivity mStatusChatActivity = StatusChatActivity.SEND_CHAT_BUTTON;
+    private Fragment mFragment;
+
+    public Vector<Chat> getMessages() {
+        return messages;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         if (savedInstanceState == null) {
+            mFragment = new PlaceholderFragment();
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, mFragment)
                     .commit();
         }
+        sendData(true);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        messages = TransportDataService.getQueueChat();
+        TransportDataService.setOnlineChat(true);
+        new Thread(new UpdateChat()).start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        TransportDataService.setOnlineChat(false);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        TransportDataService.setOnlineChat(false);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,20 +84,109 @@ public class ChatActivity extends BinderServiceActivity {
 
     @Override
     protected boolean isValid() {
-        return true;
+        boolean isValid = false;
+        switch (mStatusChatActivity) {
+            case SEND_CHAT_BUTTON:
+                isValid = true;
+                break;
+            case SEND_CHAT_MESSAGE:
+                PlaceholderFragment fragment = ((PlaceholderFragment) mFragment);
+                String message = fragment.getMessageToSend();
+                if (!message.isEmpty()) {
+                    isValid = true;
+                } else {
+                    fragment.setValidationError("Ingrese un mensaje");
+                }
+                break;
+            case SEND_OPTIONS:
+                isValid = false;
+                break;
+            default:
+                break;
+        }
+        return isValid;
     }
 
     @Override
     protected String buildData() {
-        return new MessageBuilder(this).buildMessageMainButton(Config.buttonStrings.TYPE_MESSAGE_CHAT);
+        PlaceholderFragment fragment = ((PlaceholderFragment) mFragment);
+        String data = null;
+        switch (mStatusChatActivity) {
+            case SEND_CHAT_BUTTON:
+                data = new MessageBuilder(this).buildMessageMainButton(Config.buttonStrings.TYPE_MESSAGE_CHAT);
+                break;
+            case SEND_CHAT_MESSAGE:
+                String messasge = fragment.getMessageToSend();
+                data = new co.com.widetech.mamut.android.utils.MessageBuilder(this).buildMessageToChat(messasge);
+                break;
+            case SEND_OPTIONS:
+                break;
+            default:
+                break;
+        }
+        return data;
+    }
+
+    public boolean sendMessage() {
+        mStatusChatActivity = StatusChatActivity.SEND_CHAT_MESSAGE;
+        return sendData(true);
+    }
+
+    enum StatusChatActivity {
+        SEND_CHAT_BUTTON,
+        SEND_CHAT_MESSAGE,
+        SEND_OPTIONS,
     }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+        private Button mSendButton;
+        private Button mBackButton;
+        private Button mOptionsButton;
+        private EditText mEditTextMessage;
+        private ListView mListViewChats;
 
         public PlaceholderFragment() {
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            Activity activity = getActivity();
+            mSendButton = (Button) activity.findViewById(R.id.ButtonEnviar);
+            mBackButton = (Button) activity.findViewById(R.id.ButtonVolver);
+            mOptionsButton = (Button) activity.findViewById(R.id.ButtonOpciones);
+            mEditTextMessage = (EditText) activity.findViewById(R.id.editTextMessage);
+            mListViewChats = (ListView) activity.findViewById(R.id.ChatlistView);
+            mSendButton.setOnClickListener(this);
+            mBackButton.setOnClickListener(this);
+            mOptionsButton.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int id = view.getId();
+            ChatActivity parentActivity = ((ChatActivity) getActivity());
+            switch (id) {
+                case R.id.ButtonEnviar:
+                    if (parentActivity.sendMessage()) {
+                        setValidationError(null);
+                        Chat chat = new Chat(getMessageToSend(), WideTechTools.getHourPhone(), "Conductor");
+                        parentActivity.messages.add(chat);
+                        updateMessagesChat();
+                        mEditTextMessage.setText("");
+                    }
+                    break;
+                case R.id.ButtonVolver:
+                    parentActivity.finish();
+                    break;
+                case R.id.ButtonOpciones:
+                    break;
+                default:
+                    break;
+            }
         }
 
         @Override
@@ -66,6 +194,49 @@ public class ChatActivity extends BinderServiceActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
             return rootView;
+        }
+
+        public String getMessageToSend() {
+            return mEditTextMessage.getText().toString();
+        }
+
+        public void setValidationError(String messageError) {
+            mEditTextMessage.setError(messageError);
+        }
+
+        private void updateMessagesChat() {
+            ChatActivity parentActivity = ((ChatActivity) getActivity());
+            mListViewChats.setAdapter(new ChatAdapter(parentActivity.getMessages(), parentActivity));
+            mListViewChats.setFocusable(true);
+            mListViewChats.setSelection(parentActivity.sizechat);
+        }
+    }
+
+    class UpdateChat implements Runnable {
+        private boolean service = true;
+
+        public void run() {
+            while (service) {
+                messages = TransportDataService.getQueueChat();
+                if (messages.size() > sizechat) {
+                    sizechat = messages.size();
+                    try {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                ((PlaceholderFragment) mFragment).updateMessagesChat();
+                            }
+                        });
+                    } catch (Exception ex) {
+                        String e = ex.getMessage();
+                        System.err.println(e);
+                    }
+                }
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
